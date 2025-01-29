b40d720a0e28b91210f1e10dfcd80862759bca93
dennischarlesgimeno <dennischarles.gimeno@gmail.com>
2025-01-29 18:17:24 -0500

    json format

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,4 +5,5 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    formatted_response = json.loads(response)
+    return formatted_response
