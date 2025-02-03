9cc1636d921afe485a1983c69463116411e48649
dennischarlesgimeno <dennischarles.gimeno1@gmail.com>
2025-02-02 21:31:21 -0500

    server

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detection")
+app.run(debug=True, port=5000)
+
+@app.route('/emotionDetector')
+def emotionDetector():
+    user_input = request.args.get('userinput')
+    result = emotion_detector(user_input)
+    return(f"For the given statement, the system resonse is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']}, 'sadness': {result['sadness']}, 'dominant_emotion': {result['dominant_emotion']}")
