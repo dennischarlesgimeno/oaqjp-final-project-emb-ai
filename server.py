d1df3f30fee0aad9399090399771116b1e35ad42
dennischarlesgimeno <dennischarles.gimeno1@gmail.com>
2025-02-02 23:43:13 -0500

    server debug

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("__name__")
-app.run(debug=True, port=5000)
+app = Flask(__name__)
+
+# to define route for home page
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 #to define route for emotionDetector
 @app.route('/emotionDetector')
@@ -10,3 +14,6 @@ def emotionDetector():
     user_input = request.args.get('userinput')
     result = emotion_detector(user_input)
     return(f"For the given statement, the system resonse is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']}, 'sadness': {result['sadness']}, 'dominant_emotion': {result['dominant_emotion']}")
+
+# if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=5000)
