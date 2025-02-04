2620666b3b40996fbf40f8bdcc5e08151eb996f7
dennischarlesgimeno <dennischarles.gimeno1@gmail.com>
2025-02-03 21:37:31 -0500

    working server

--- server.py
+++ server.py
@@ -11,9 +11,9 @@ def home():
 #to define route for emotionDetector
 @app.route('/emotionDetector')
 def emotionDetector():
-    user_input = request.args.get('userinput')
+    user_input = request.args.get('textToAnalyze')
     result = emotion_detector(user_input)
     return(f"For the given statement, the system resonse is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']}, 'sadness': {result['sadness']}, 'dominant_emotion': {result['dominant_emotion']}")
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
