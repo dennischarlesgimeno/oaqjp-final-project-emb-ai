797b1c56960cedaf409a1b1a465e64601c09234a
dennischarlesgimeno <dennischarles.gimeno1@gmail.com>
2025-02-05 21:31:11 -0500

    final

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""This module contains the codes for the server including error handling"""
+
 from flask import Flask, request, render_template, jsonify
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,7 +7,8 @@ app = Flask(__name__)
 
 # error handler for 400
 @app.errorhandler(400)
-def bad_request_error(error):
+def bad_request_error():
+    """To handle 400 Bad Request errors"""
     response = jsonify({'error': 'Bad Request', 'message': 'Invalid text! Please try again!'})
     response.status_code = 400
     return response
@@ -13,25 +16,24 @@ def bad_request_error(error):
 # to define route for home page
 @app.route('/')
 def home():
+    """Render the home page"""
     return render_template('index.html')
 
-#to define route for emotionDetector
+# Define route for emotionDetector
 @app.route('/emotionDetector')
-def emotionDetector():
-    try:
-        user_input = request.args.get('textToAnalyze')
-        result = emotion_detector(user_input)
-        response = (
-        f"For the given statement, the system resonse is 'anger': {result['anger']}, "
-        f"'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']}, "
-        f"'sadness': {result['sadness']}, 'dominant_emotion': {result['dominant_emotion']}"
-        )
-        if result['dominant_emotion'] == None:
-            return "Invalid text! Please try again!"
-        return response
-
-    except:
+def emotion_detector_serv():
+    """handle emotion detection requests"""
+    user_input = request.args.get('textToAnalyze')
+    result = emotion_detector(user_input)
+    response = (
+    f"For the given statement, the system resonse is 'anger': {result['anger']}, "
+    f"'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']}, "
+    f"'sadness': {result['sadness']}, 'dominant_emotion': {result['dominant_emotion']}"
+    )
+    if result['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
+    return response
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
