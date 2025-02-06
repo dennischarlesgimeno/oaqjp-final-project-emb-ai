077e3d29aa57b4fc97fec261e92cdaeac07bb5d7
dennischarlesgimeno <dennischarles.gimeno1@gmail.com>
2025-02-05 21:07:50 -0500

    working error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,27 +1,43 @@
 import requests, json
 
 def emotion_detector(text_to_analyse):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    myobj = { "raw_document": { "text": text_to_analyse } }
-    response = requests.post(url, json = myobj, headers=header)
-    formatted_response = json.loads(response.text)
-    emotion_predictions = formatted_response['emotionPredictions'][0]
-    emotion = emotion_predictions['emotion']
-    dominant_emotion = max(emotion, key=emotion.get)
+    try:
+        url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+        header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+        myobj = { "raw_document": { "text": text_to_analyse } }
+        response = requests.post(url, json = myobj, headers=header)
+        if response.status_code == 200:
+            formatted_response = json.loads(response.text)
+            emotion_predictions = formatted_response['emotionPredictions'][0]
+            emotion = emotion_predictions['emotion']
+            dominant_emotion = max(emotion, key=emotion.get)
     
-    ## to assign scores to each label
-    anger_score = emotion['anger']
-    disgust_score = emotion['disgust']
-    fear_score = emotion['fear']
-    joy_score = emotion['joy']
-    sadness_score = emotion['sadness']
+            ## to assign scores to each label
+            anger_score = emotion['anger']
+            disgust_score = emotion['disgust']
+            fear_score = emotion['fear']
+            joy_score = emotion['joy']
+            sadness_score = emotion['sadness']
 
-    return {
-    'anger': anger_score, 
-    'disgust': disgust_score, 
-    'fear': fear_score, 
-    'joy': joy_score, 
-    'sadness': sadness_score, 
-    'dominant_emotion': dominant_emotion
-    }
+            return {
+            'anger': anger_score, 
+            'disgust': disgust_score, 
+            'fear': fear_score, 
+            'joy': joy_score, 
+            'sadness': sadness_score, 
+            'dominant_emotion': dominant_emotion
+            }
+        elif response.status_code == 400:
+            value = {
+            'anger': None, 
+            'disgust': None, 
+            'fear': None, 
+            'joy': None, 
+            'sadness': None, 
+            'dominant_emotion': None
+            }
+            return value
+
+    except KeyError:
+        return "KeyError"
+        
--- server.py
+++ server.py
@@ -1,7 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
+
+# error handler for 400
+@app.errorhandler(400)
+def bad_request_error(error):
+    response = jsonify({'error': 'Bad Request', 'message': 'Invalid text! Please try again!'})
+    response.status_code = 400
+    return response
 
 # to define route for home page
 @app.route('/')
@@ -11,9 +18,20 @@ def home():
 #to define route for emotionDetector
 @app.route('/emotionDetector')
 def emotionDetector():
-    user_input = request.args.get('textToAnalyze')
-    result = emotion_detector(user_input)
-    return(f"For the given statement, the system resonse is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']}, 'sadness': {result['sadness']}, 'dominant_emotion': {result['dominant_emotion']}")
+    try:
+        user_input = request.args.get('textToAnalyze')
+        result = emotion_detector(user_input)
+        response = (
+        f"For the given statement, the system resonse is 'anger': {result['anger']}, "
+        f"'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']}, "
+        f"'sadness': {result['sadness']}, 'dominant_emotion': {result['dominant_emotion']}"
+        )
+        if result['dominant_emotion'] == None:
+            return "Invalid text! Please try again!"
+        return response
+
+    except:
+        return "Invalid text! Please try again!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
